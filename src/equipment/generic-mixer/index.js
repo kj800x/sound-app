358da0c83b7d1261d0fa280db3819bde8a6e24a4
Kevin Johnson <kevin@coolkev.com>
2020-02-17 00:15:53 -0500

    Separate port groups for I/O on mixers

--- src/equipment/generic-mixer/index.js
+++ src/equipment/generic-mixer/index.js
@@ -4,12 +4,17 @@ export default {
   type: "generic-mixer",
   portGroups: [
     {
-      location: [0.5, 0],
+      location: [0.2, 0],
       connectors: {
         "IN 1": "xlr",
         "IN 2": "xlr",
         "IN 3": "xlr",
         "IN 4": "xlr",
+      }
+    },
+    {
+      location: [0.8, 0],
+      connectors: {
         "MAIN L": "speakon",
         "MAIN R": "speakon"
       }
